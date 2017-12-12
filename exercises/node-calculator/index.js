6c10d1dc447a035894239945afbe3fea6b5165a9
V School Student <erica.stone313@gmail.com>
2017-12-12 15:43:53 -0700

    finished JS node calculator assignment

--- exercises/node-calculator/index.js
+++ exercises/node-calculator/index.js
@@ -7,5 +7,33 @@ function sum(num1,num2) {
     return Number(num1) + Number(num2);
 }
 
-console.log(sum(firstNum, secondNum));
+function sub(num1,num2) {
+    return Number(num1) - Number(num2);
+}
+
+function mul(num1,num2) {
+    return Number(num1) * Number(num2)
+}
+
+function div(num1,num2) {
+    return Number(num1) / Number(num2)
+}
+
+var operations = ["add", "subtract", "multiply", "divide"];
+
+var operation = rs.keyInSelect(operations, "\nWhich operation would you like to perform with these numbers?" );
+
+console.log("The result of your calculation is: " +calculate())
+
+function calculate() {
+    if (operation === 0) {
+        return sum(firstNum,secondNum)
+    } else if (operation === 1) {
+        return sub(firstNum,secondNum)
+    } else if (operation === 2) {
+        return mul(firstNum,secondNum) 
+    } else if (operation === 3) {
+        return div(firstNum,secondNum)
+    } else {return "Game Over.  Bye."}
+}
 
